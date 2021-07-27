1650ee3e7677e72e648e2fbe045e7eada9b3b00a
victor <victordronov1@gmail.com>
2021-07-27 19:32:38 -0400

    changes applied

--- src/atoms/Toast/Toast.tsx
+++ src/atoms/Toast/Toast.tsx
@@ -1,6 +1,7 @@
 import { classnames } from "@story-squad/react-utils";
 import React from "react";
 import { FaRegCheckCircle } from "react-icons/fa";
+import { FiAlertCircle, FiX, FiXCircle } from "react-icons/fi";
 import { ToastProps } from "react-toast-notifications";
 import "./toast.scss";
 import ToastTitle from "./ToastTitle";
@@ -8,11 +9,20 @@ import ToastTitle from "./ToastTitle";
 const Toast = ({ appearance, children }: ToastProps): React.ReactElement => {
   return (
     <div className={classnames("toast", appearance)}>
-      <FaRegCheckCircle />
       <div className="text-wrapper">
-        <ToastTitle appearance={appearance} />
+        <div className="icon-title-container">
+          {appearance === "success" ? (
+            <FaRegCheckCircle size={20} />
+          ) : appearance === "error" ? (
+            <FiXCircle size={20} />
+          ) : (
+            <FiAlertCircle size={20} />
+          )}
+          <ToastTitle appearance={appearance} />
+        </div>
         <p className="message">{children}</p>
       </div>
+      <FiX size={14} color="#878888" />
     </div>
   );
 };
--- src/atoms/Toast/ToastTitle.tsx
+++ src/atoms/Toast/ToastTitle.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { ToastProps } from "react-toast-notifications";
 
 const ToastTitle = ({ appearance }: Pick<ToastProps, "appearance">) => {
-  return (
-    <div className="title">
-      {appearance}
-    </div>
-  );
+  return <div className="title">{appearance}</div>;
 };
 
 export default ToastTitle;

c5fec05352d6c6340fb8eaef6848f9e31a9e8828
Brandon <bran.ramirez.don@gmail.com>
2021-08-25 07:55:52 -0700

    Caps and style folder

--- src/atoms/InputLabel/InputLabel.tsx
+++ src/atoms/InputLabel/InputLabel.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { FiHelpCircle } from "react-icons/fi";
-import { ILabelProps } from "./InputLabel.model";
-import "./inputlabel.scss";
+import React from 'react';
+import { FiHelpCircle } from 'react-icons/fi';
+import { ILabelProps } from './InputLabel.model';
+import './styles/index.scss';
 
 const Label = ({
   label,
@@ -10,9 +10,9 @@ const Label = ({
 }: ILabelProps): React.ReactElement => {
   return (
     <label className="label">
-      {labelType === "required" && <span className="require">*</span>}
-      {label} {toolTip ? <FiHelpCircle className="optional" /> : ""}
-      {labelType === "optional" && <span className="optional">(optional)</span>}
+      {labelType === 'required' && <span className="require">*</span>}
+      {label} {toolTip ? <FiHelpCircle className="optional" /> : ''}
+      {labelType === 'optional' && <span className="optional">(optional)</span>}
     </label>
   );
 };
